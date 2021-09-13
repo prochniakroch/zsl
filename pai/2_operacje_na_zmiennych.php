976171f944e31624ee3a2fd39b3a1edd4863c563
prochniakroch <90000844+prochniakroch@users.noreply.github.com>
2021-09-13 09:34:10 +0200

    Add files via upload

--- pai/2_operacje_na_zmiennych.php
+++ pai/2_operacje_na_zmiennych.php
@@ -29,4 +29,32 @@
     } else {
         echo "różne";
     }
+
+    /*
+    postinkremetacja $x++   
+    preinkrementacja ++$x
+    postdekremetacja $x--
+    predekrementacja --$x
+    */
+
+    $x=2;
+    echo "<hr>$x"; //2
+    echo ++$x; //3
+    echo $x++; //3
+    echo ++$x."<br>"; //5
+
+    $x=1;
+    echo $x; //1
+    $x=$x++;
+    echo $x; //1
+    $x=++$x;
+    echo $x; //2
+    $y=++$x;
+    echo $x; //3
+    echo $y; //3
+    $y=$x++;
+    echo $x; //4
+    echo $y."<br>"; //3 
+
+    
 ?>
