734a9603ac44b29138b41d33f83fe62035ab416e
prochniakroch <90000844+prochniakroch@users.noreply.github.com>
2021-10-25 10:40:13 +0200

    Delete pai/6funkcje directory

--- pai/6funkcje/script/6_1script.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if(!empty($_POST['name'])){
-    require_once '../funkcje/1_function.php';
-    //echo text();
-    echo name('Krystyna').'<hr>';
-    echo 'imie:'.validateString($_POST['name'],($_POST['lenght']));
-
-}
-  ?>
